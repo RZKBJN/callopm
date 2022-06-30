0756a6cc1efae675244fe8e4e4abf2d49508933b
RZKBJN <108453961+RZKBJN@users.noreply.github.com>
2022-06-30 13:07:34 +0200

    buildah

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 
 #rm -r out/
 echo "--------------- STEP 1 ------------"
-docker run -w /proj -v $PWD:/proj  openresty/openresty:bionic bash /proj/call-opm.sh
+buildah run -w /proj -v $PWD:/proj  openresty/openresty:bionic bash /proj/call-opm.sh
