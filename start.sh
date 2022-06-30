0b3114531b211422d0bf48401fcabed8a2732056
RZKBJN <108453961+RZKBJN@users.noreply.github.com>
2022-06-30 13:09:59 +0200

    w raus

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 
 #rm -r out/
 echo "--------------- STEP 1 ------------"
-buildah run -w /proj -v $PWD:/proj  openresty/openresty:bionic bash /proj/call-opm.sh
+buildah run -v $PWD:/proj  openresty/openresty:bionic bash /proj/call-opm.sh
