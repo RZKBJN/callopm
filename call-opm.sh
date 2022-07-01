555c5a91e3f1e33160d09ae15d946a15f4e7c6ac
RZKBJN <108453961+RZKBJN@users.noreply.github.com>
2022-07-01 09:22:43 +0200

    vers

--- call-opm.sh
+++ call-opm.sh
@@ -5,4 +5,4 @@ mkdir /proj/out
 mkdir /proj/out/opm
 
 cd /proj/out/opm || exit 10
-opm -cwd install zmartzone/lua-resty-openidc=1.7.4 fffonion/lua-resty-openssl=0.7.3 openresty/lua-resty-redis=0.27
+opm -cwd install zmartzone/lua-resty-openidc fffonion/lua-resty-openssl openresty/lua-resty-redis
