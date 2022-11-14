6d1a5830c81c2d0de993076c5411e59cec04c26a
RZKBJN <108453961+RZKBJN@users.noreply.github.com>
2022-11-14 10:46:03 +0100

    Update call-opm.sh
    ktalebian/lua-resty-cookie

--- call-opm.sh
+++ call-opm.sh
@@ -5,5 +5,6 @@ mkdir /proj/out
 mkdir /proj/out/opm
 
 cd /proj/out/opm || exit 10
-opm -cwd install zmartzone/lua-resty-openidc fffonion/lua-resty-openssl openresty/lua-resty-redis ledgetech/lua-resty-redis-connector
+opm -cwd install zmartzone/lua-resty-openidc fffonion/lua-resty-openssl openresty/lua-resty-redis ledgetech/lua-resty-redis-connector ktalebian/lua-resty-cookie
+
 
